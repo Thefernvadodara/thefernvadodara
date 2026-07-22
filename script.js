ad7e712c8ba81a5662219ceef119512ea732b1aa
Thefernvadodara <hardikrangani43@gmail.com>
2026-07-22 12:16:27 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".btn").forEach(button=>{
-    button.addEventListener("mouseenter",()=>{
-        button.style.transform="scale(1.03)";
-    });
-
-    button.addEventListener("mouseleave",()=>{
-        button.style.transform="scale(1)";
-    });
-});
