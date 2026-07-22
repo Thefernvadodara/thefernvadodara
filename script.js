8dea84d4487f8bcf978a363f75cd2cc4c70db7d0
Thefernvadodara <hardikrangani43@gmail.com>
2026-07-22 11:23:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".btn").forEach(button=>{
+    button.addEventListener("mouseenter",()=>{
+        button.style.transform="scale(1.03)";
+    });
+
+    button.addEventListener("mouseleave",()=>{
+        button.style.transform="scale(1)";
+    });
+});
